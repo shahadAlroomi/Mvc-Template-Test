321ece278b5208e94c68b40b8884cb3d21b708f6
shahadAlroomi <shahad.ro@icloud.com>
2026-02-04 10:09:32 +0100

    other way to define the welcome method

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,8 +13,8 @@ public class HelloWorldController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int numTimes = 1)
-{
-    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-}
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
 }
