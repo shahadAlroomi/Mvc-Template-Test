766fdd1d028a17ed9a89be0fe49798ad199c478b
shahadAlroomi <shahad.ro@icloud.com>
2026-02-04 11:42:42 +0100

    Part 5work with a database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 if (builder.Environment.IsDevelopment())
 {
@@ -19,13 +21,22 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//Add the seed initializer
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-// }
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
